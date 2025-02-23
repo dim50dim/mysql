5c4ae68d30c8f90e1d8b192fc19ef3b5ace2cb13
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-23 08:54:30 +0100

    find min price of the car

--- task_07.js
+++ task_07.js
@@ -11,5 +11,8 @@ const CONFIG            = require('./config');
 const task_01 = require('./task_01');
 
 module.exports = function () {
+    const result = task_01('SELECT MIN(cost) as price FROM cars');
+    const min = result.map(({price}) => price);
+    return min;
 }
 
